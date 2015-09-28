e3b85e0a0020d738f554599f5c56bb68b3f6108c
Noah Rosamilia <ivoahivoah@gmail.com>
2015-09-27 22:39:24 -0400

    Initial config file support

--- PyBot.py
+++ PyBot.py
@@ -1,36 +1,21 @@
-import socket
+import socket, json
 
 class PyBot:
-    def __init__(self, nick, server, channels):
-        self.nick = nick
+    def __init__(self, conf):
         self.irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.dict = {}
-        if len(server) != 2: server = (server, 6667) #If we don't have a port number, use 6667
-        self.server = server
-        if isinstance(channels, basestring): channels = [channels] #If we only have one channel, then make it a list for self.connect
-        self.channels = channels
-        self.debug = False
-        self.bots = []
-        self.verbose = False
+        self.factoids = {}
+        self.conf = json.load(open(conf))
+        self.loadFactoids(self.conf['factoids'])
 
     def connect(self):
-        self.irc.connect(self.server)
-        self.irc.send('NICK ' + self.nick + '\r\n') #Set nickname
-        self.irc.send('USER ' + self.nick + ' 8 * :' + self.nick + '\r\n') #Set username
+        self.irc.connect(tuple(self.conf['server']))
+        self.irc.send('NICK ' + self.conf['name'] + '\r\n')
+        self.irc.send('USER ' + self.conf['name'] + ' 8 * :' + self.conf['name'] + '\r\n')
         while True:
-            data = self.process()
-            if 'End of /MOTD command' in data: break
-        self.irc.send('JOIN ' + ','.join(self.channels) + '\r\n') #Join channels
-
-    def setDebug(self, debug):
-        self.debug = debug
-
-    def setBots(self, bots):
-        if isinstance(bots, basestring): self.bots = [bots]
-        else: self.bots = bots
-
-    def setVerbose(self, verbose):
-        self.verbose = verbose
+            data = self.irc.recv(512)
+            if self.conf['debug']: print data
+            if '/MOTD' in data: break
+        self.irc.send('JOIN ' + ','.join(self.conf['channels']) + '\r\n')
 
     def loadFactoids(self, filename):
         self.filename = filename
@@ -38,7 +23,7 @@ class PyBot:
         for factoid in factoids:
             text = '|'.join(factoid.split('|')[1:])
             text = text.replace('<action> ', '\x01ACTION ').replace('<action>', '\x01ACTION ').replace('<reply> ', '').replace('<reply>', '')
-            self.dict[factoid.split('|')[0].lower()] = text
+            self.factoids[factoid.split('|')[0].lower()] = text
 
     def process(self):
         try:
@@ -48,7 +33,7 @@ class PyBot:
         except socket.error:
             return ''
 
-        if self.debug: print(raw_message)
+        if self.conf['debug']: print(raw_message)
         if raw_message[0] == ':':
             nick = raw_message.split('!')[0][1:]
             message = raw_message.split()[1:]
@@ -64,33 +49,29 @@ class PyBot:
             channel = message[0]
             message = message[1:]
             message[0] = message[0][1:]
-            if nick in self.bots: i = 1
+            if nick in self.conf['bots']: i = 1
             else: i = 0
             if message[i][0] == '~':
-                if channel == self.nick: channel = nick
+                if channel == self.conf['name']: channel = nick
                 try:
-                    if self.debug: print('PRIVMSG ' + channel + ' :' + self.dict[message[i][1:].lower()].replace('$nick', nick) + '\r\n')
-                    self.irc.send('PRIVMSG ' + channel + ' :' + self.dict[message[i][1:].lower()].replace('$nick', nick) + '\r\n')
+                    if self.conf['debug']: print('PRIVMSG ' + channel + ' :' + self.factoids[message[i][1:].lower()].replace('$nick', nick) + '\r\n')
+                    self.irc.send('PRIVMSG ' + channel + ' :' + self.factoids[message[i][1:].lower()].replace('$nick', nick) + '\r\n')
                 except KeyError:
-                    if self.verbose: self.irc.send('PRIVMSG ' + channel + ' : I don\'t understand the command \'' + message[i][1:] + '\'\r\n')
+                    if self.conf['verbose']: self.irc.send('PRIVMSG ' + channel + ' : I don\'t understand the command \'' + message[i][1:] + '\'\r\n')
 
         return raw_message
 
-    def close(self):
-        self.irc.send('QUIT ' + self.nick + ' has better things to do\r\n')
+    def disconnect(self):
+        self.irc.send('QUIT :' + self.conf['name'] + ' has better things to do\r\n')
         self.irc.close()
 
 if __name__ == '__main__':
-    bot = PyBot('IvoBot', 'irc.eversible.net', ['#cemetech', '#flood'])
-    bot.setBots('saxjax')
-    bot.setDebug(True)
-    bot.setVerbose(False)
+    bot = PyBot('config.json')
     bot.connect()
-    bot.loadFactoids('factoids.txt')
     while True:
         try:
             bot.process()
         except KeyboardInterrupt:
-            bot.close()
+            bot.disconnect()
             print
             break
